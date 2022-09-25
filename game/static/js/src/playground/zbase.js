73de8785369099d79084823704f169470def0876
bugmaker24 <769877269@qq.com>
2022-09-25 18:27:57 +0800

    write menu and playground scene

---
+++ game/static/js/src/playground/zbase.js
@@ -0,0 +1,20 @@
+class AcGamePlayground {
+        constructor(root){
+            this.root = root;
+            this.$playground = $(`<div>游戏界面</div>`);
+
+            this.root.$ac_game.append(this.$playground);
+        }
+    start(){
+
+    }
+
+
+    show(){  //打开playground界面
+        this.$playground.show();
+    }
+
+    hide(){  //关闭playground界面
+        this.$playground.hide();
+    }
+}
