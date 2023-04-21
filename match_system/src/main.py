7c4711fb2cc0e1b12b8e73771e2a7b92e8ac2dc7
bugmaker24 <769877269@qq.com>
2023-04-21 11:21:54 +0800

    implement thrift service

--- match_system/src/main.py
+++ match_system/src/main.py
@@ -13,6 +13,10 @@ from queue import Queue
 from time import sleep
 from threading import Thread
 
+from acapp.asgi import channel_layer
+from asgiref.sync import async_to_sync
+from django.core.cache import cache
+
 queue = Queue()  # 消息队列
 
 class Player:
@@ -39,6 +43,28 @@ class Pool:
 
     def match_success(self, ps):
         print("Match Success: %s %s %s" % (ps[0].username, ps[1].username, ps[2].username))
+        room_name = "room-%s-%s-%s" % (ps[0].uuid, ps[1].uuid, ps[2].uuid)
+        players = []
+        for p in ps:
+            async_to_sync(channel_layer.group_add)(room_name, p.channel_name)
+            players.append({
+                'uuid': p.uuid,
+                'username': p.username,
+                'photo': p.photo,
+                'hp': 100,
+            })
+        cache.set(room_name, players, 3600)  #有效时间1小时
+        for p in ps:
+            async_to_sync(channel_layer.group_send)(
+                room_name,
+                {
+                    'type': "group_send_event",
+                    'event': "create_player",
+                    'uuid': p.uuid,
+                    'username': p.username,
+                    'photo': p.photo,
+                }
+            )
 
 
     def increase_waiting_time(self):
@@ -48,6 +74,7 @@ class Pool:
     def match(self):
         while len(self.players) >=3:
             self.players = sorted(self.players, key=lambda p: p.score)
+            flag = False
             for i in range(len(self.players) - 2):
                 a, b, c = self.players[i], self.players[i+1], self.players[i+2]
                 if self.check_match(a, b) and self.check_match(b, c) and self.check_match(a, c):
